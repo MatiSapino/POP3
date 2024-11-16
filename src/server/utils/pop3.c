cb71124e38f02fd6f5ada9a7320b8d9b401abab6
MatiSapino <msapino@itba.edu.ar>
2024-11-16 16:04:59 -0300

    Feat: add error response

--- src/server/utils/pop3.c
+++ src/server/utils/pop3.c
@@ -3,112 +3,6 @@
 #include <commandParse.h>
 #include <pop3_commands.h>
 
-//struct command_function {
-//    char *name;
-//    enum pop3_state (*function)(struct selector_key *key, struct command *command);
-//};
-///*
-
-//
-//
-// static const char* maildir;
-// static Client connections[MAX_CLIENTS] = {0};
-// #define OK_RESPONSE(m) OK_RESP " " m ENTER
-// #define ERR_RESPONSE(m) ERR_RESP " " m ENTER
-// //
-// //
-// //// Función para generar una respuesta OK
-// const char* ok_respon(const char *message) {
-//    static char response[MAX_POP3_RESPONSE_LENGTH];
-//    snprintf(response, sizeof(response), "%s %s%s", OK_RESP, message, ENTER);
-//    return response;
-// }
-
-// //// Función para generar una respuesta de error
-// const char* err_respon(const char *message) {
-//    static char response[MAX_POP3_RESPONSE_LENGTH];
-//    snprintf(response, sizeof(response), "%s %s%s", ERR_RESP, message, ENTER);
-//    return response;
-// }
-
-
-// // Setea la ruta del directorio de mails, usando un valor por defecto si no se pasa un argumento.
-// static void set_maildir(const char *dir) {
-//    maildir = dir ? dir : "./dist/mail";
-// }
-
-// //// Crea el directorio de mails si no existe.
-// static void create_maildir_if_needed() {
-//    if (access(maildir, F_OK) == -1) {
-//        mkdir(maildir, S_IRWXU);
-//    }
-// }
-
-// static  void reset_connections() {
-//    for (size_t i = 0; i < MAX_CLIENTS; i++) {
-//        connections[i].username[0] = 0;
-//        connections[i].authenticated = false;
-//    }
-// }
-
-// void pop_init(char *dir) {
-//    set_maildir(dir);
-//    create_maildir_if_needed();
-//    reset_connections();
-// }
-
-// int initialize_pop_connection(int sock_fd, struct sockaddr_in client_address)
-// {
-//    Client *client_connection = &connections[sock_fd];
-//    client_connection->username[0] = '\0';
-//    client_connection->authenticated = false;
-
-//    const char welcome_msg[] = OK_RESPONSE(" POP3 server ready");
-//    ssize_t sent_bytes = send(sock_fd, welcome_msg, strlen(welcome_msg), 0);
-
-//    if (sent_bytes < 0)
-//    {
-//        fprintf(stderr, "Error sending welcome message to client\n");
-//        return -1; // Error
-//    }
-
-//    return 0; // Success
-// }
-
-// //// Manejo del comando STAT
-// void handle_stat(Client *client) {
-//    size_t size = 0, count = 0;
-
-//    for (Mailfile *mail = client->mails; mail->mailid[0]; mail++) {
-//        if (!mail->deleted) {
-//            size += mail->size;
-//            count++;
-//        }
-//    }
-
-//    char buffer[MAX_POP3_RESPONSE_LENGTH];
-//    snprintf(buffer, sizeof(buffer), OK_RESPONSE("%zu %zu"), count, size);
-//    send(client->socket_fd, buffer, strlen(buffer), 0);
-//    return;
-// }
-
-// void parse_command(char * buf, int socket) {
-//     return;
-// }
-
-// void handle_list(Client * client){
-//     return;
-// }
-// void handle_quit(Client * client){
-//     return;
-// }
-// void handle_dele(Client * client,char * buffer){
-//     return;
-// }
-// void handle_retr(Client * client,char * buffer){
-//     return;
-// }
-
 static unsigned welcomeClient(struct selector_key * selector_key) {
     struct Client * client = selector_key->data;
 
@@ -141,6 +35,16 @@ static unsigned welcomeClient(struct selector_key * selector_key) {
 handle_error:
     status = selector_set_interest_key(selector_key, OP_NOOP);
     return STATE_ERROR;
+}
+
+void errResponse(struct Client * client, const char * message) {
+    size_t limit;
+    uint8_t * buffer;
+    ssize_t count;
+
+    buffer = buffer_write_ptr(&client->outputBuffer, &limit);
+    count = snprintf((char *) buffer, limit, "-ERR %s\r\n", message);
+    buffer_write_adv(&client->outputBuffer, count);
 }
 
 void okResponse(struct Client * client, const char * message) {
